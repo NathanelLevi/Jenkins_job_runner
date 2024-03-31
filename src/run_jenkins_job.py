78266bd145546708d254a4f33c67281e3160e02c
nathanell <nathanel.levi@kenshoo.com>
2024-03-31 10:56:12 +0300

    test3

--- src/run_jenkins_job.py
+++ src/run_jenkins_job.py
@@ -17,7 +17,7 @@ class JenkinsJob:
                                                    password=self._jenkins_password)
     
 
-    def _get_job_params_in_dict(s):
+    def _get_job_params_in_dict(self):
         if os.getenv('INPUT_PARAMETERS') and os.getenv('INPUT_PARAMETERS') != '' :
             return json.loads(os.getenv('INPUT_PARAMETERS'))
         else:
@@ -25,27 +25,27 @@ class JenkinsJob:
         
     @retry(ValueError, delay=20)
     def _wait_for_build_number(self, queued_item_id):
-        queue_item_data = self.jenkins_connection.get_queue_item(queued_item_id)
+        queue_item_data = self._jenkins_connection.get_queue_item(queued_item_id)
         if 'executable' in queue_item_data:
             return queue_item_data['executable']['number']
         raise ValueError('Not correct Build Number')
 
     @retry(ValueError, delay=20)
     def _wait_for_job_to_start(self, queued_item_id):
-        if self.jenkins_connection.get_queue_item(queued_item_id)['why'] is None:
+        if self._jenkins_connection.get_queue_item(queued_item_id)['why'] is None:
             return True
         raise ValueError('Not correct Build Queue status')
 
     @retry(ValueError, delay=60)
     def _get_build_result(self, build_number):
-        build_info = self.jenkins_connection.get_build_info(name=self._job_name, number=build_number)
+        build_info = self._jenkins_connection.get_build_info(name=self._job_name, number=build_number)
         if not build_info['building']:
             return build_info['result']
         else:
             raise ValueError('Not correct Build Info status')
         
     def _build_jenkins_job(self):
-            return self.jenkins_connection.build_job(name=self._job_name, parameters=self._parameters)
+            return self._jenkins_connection.build_job(name=self._job_name, parameters=self._parameters)
 
     def trigger_jenkins_job(self):
         try:
